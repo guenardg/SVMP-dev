8b0793b9f0d395fad3023abecd926dab82101fe3
guenardg <guillaume.guenard@gmail.com>
2020-03-11 13:12:55 -0400

    Added the circular case to the idea about treating asymmetrical process using Hermitian eigenfunctions

--- Test/Asymmetry/Asymmetry.R
+++ Test/Asymmetry/Asymmetry.R
@@ -19,6 +19,8 @@ tol <- .Machine$double.eps^0.5
 m <- matrix(0+0i,N,N)
 m[cbind(1L:(N-1L),2L:N)] <- complex(mod=1,arg=pi/50)
 m[cbind(2L:N,1L:(N-1L))] <- complex(mod=1,arg=-pi/50)
+## m[1L,N] <- complex(mod=1,arg=-pi/50)
+## m[N,1L] <- complex(mod=1,arg=pi/50)
 mm <- list(row=rowMeans(m),col=colMeans(m),mean=mean(m))
 ##
 mc <- t(t(m) - mm$col) - mm$row + mm$mean
@@ -38,7 +40,7 @@ while(i <= length(emc$values)) {
   plot(Re(emc$vectors[,i]),type="l",
        ylim=max(abs(range(Re(emc$vectors[,i]),Im(emc$vectors[,i]))))*c(-1,1))
   lines(Im(emc$vectors[,i]),col="red")
-  if(is.null(locator(1L))) break
+  ## if(is.null(locator(1L))) break
   dev.copy2eps(file=sprintf("./Image/U_%02d.eps",i))
   i <- i + 1L
 }
@@ -54,4 +56,27 @@ plot(y, type="l", xlab="Location", ylab="Response")
 par(mar=c(4.5,5,1,2))
 plot(Mod(b[-1L]), type="l", xlab="Index", ylab="Power")
 dev.copy2eps(file="./Image/Signal_power.eps")
+##
+i <- 1L
+par(mfrow=c(2L,1L))
+for(a in seq(0, 2*pi, length.out=100L)) {
+  par(mar=c(1,5,4,2))
+  plot(emc$values, type="l", xaxt="n", main=sprintf("U[%d]",i))
+  points(x=i,y=emc$values[i], pch=21L, bg="black")
+  par(mar=c(5,5,1,2))
+  bi <- complex(mod=1, arg=a)
+  yhat <- emc$vectors[,i,drop=FALSE] %*% as.matrix(bi)
+  plot(Re(yhat), type="l", ylim=c(-0.25,0.25))
+  lines(Im(yhat), col="red", lty=3L)
+  Sys.sleep(0.1)
+}
+##
+par(mfrow=c(1L,1L))
+plot(b)
+##
+### Works as expected.
+##
+### The set of coefficients b is not conjugate complementary from end to end, as
+### is the case for FT's orthogonal bases.
+###
 ##
